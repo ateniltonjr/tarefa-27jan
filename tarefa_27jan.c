66414f62ebf98df31294e2786ba80ba784e83080
ateniltonjr <ateniltonjr09@gmail.com>
2025-02-01 16:59:33 -0300

    Melhorando o layout do diagram.json.

--- tarefa_27jan.c
+++ tarefa_27jan.c
@@ -1,32 +1,39 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
+/*
+por: Atenilton Santos de Souza Júnior
+*/
+
+//Bibliotecas utilizadas
+#include <stdio.h> // Padrão da linguagem C
+#include "pico/stdlib.h" //Padrão do raspberry pi pico
 #include "hardware/pio.h"
-#include "ws2818b.pio.h"
+#include "ws2818b.pio.h" //Utilizada para configuração da matriz de leds
 
 // Definindo os pinos dos componentes
 #define PINO_MATRIZ 7  // Pino de controle da matriz de LEDs
 #define NUM_LEDS 25    // Número total de LEDs na matriz
 #define BOTAO_A 5      // Pino do Botão A
 #define BOTAO_B 6      // Pino do Botão B
-#define BLUE 11    // Pino do LED azul
-#define GREEN 12   // Pino do LED verde
-#define RED 13 // Pino do LED vermelho
+#define BLUE 11        // Pino do LED azul
+#define GREEN 12         // Pino do LED verde
+#define RED 13         // Pino do LED vermelho
 
 
 //Variável para controlhar o brilho em porcentagem
-float BRILHO = 255*0.3;
+//uint8_t (8 bits <=> 256 valores)
+uint8_t BRILHO = (255*5)/100;
 
 int num_eventos = 10; // Número de eventos para alternar entre os botões
-int estado_atual = 0; // Estado atual do sistema (0 - Desliga, 1 - Chuva)
+int estado_atual = 0; // Estado atual do sistema que será incrementado ou decrementado.
 
 // Tempo mínimo entre interrupções para debouncing
-#define DEBOUNCE_DELAY 40 // Milissegundos
+#define DEBOUNCE_DELAY 210 // Milissegundos
 
 // Variáveis para controlar o debouncing
 volatile uint32_t last_irq_time_A = 0;
 volatile uint32_t last_irq_time_B = 0;
 
-// Função para converter as posições (x, y) da matriz para um índice do vetor de LEDs
+/* Função para converter as posições (x, y) da matriz que será utilizada 
+para desenhar os números um índice do vetor de LEDs */
 int getIndex(int x, int y) {
     if (x % 2 == 0) {
         return 24 - (x * 5 + y);
@@ -37,33 +44,38 @@ int getIndex(int x, int y) {
 
 //Inicializar os pinos do led RGB
 void iniciar_rgb() {
+  //Inicializa
   gpio_init(RED);
   gpio_init(GREEN);
   gpio_init(BLUE);
-
+  //configura como saída
   gpio_set_dir(RED, GPIO_OUT);
   gpio_set_dir(GREEN, GPIO_OUT);
   gpio_set_dir(BLUE, GPIO_OUT);
 }
 
-// Função para iniciar os leds RGB dos pinos 11, 12 e 13 e configurar o tempo ligado
+// Função para acionar leds RGB e configurar o tempo ligado
 void state(bool r, bool g, bool b, int tempo) {
- iniciar_rgb();
+ iniciar_rgb(); //Chamando a função iniciar_RGB
  gpio_put(RED, r);
  gpio_put(GREEN, g);
  gpio_put(BLUE, b);
  sleep_ms(tempo);
+ /* podemos acionar os leds com: state(r, g, b, tempo);
+    Onde r, g, b são números binários que representam nível lógico alto ou baixo
+    tempo é o intervalo em milissegundos que os leds ficaram acesos. */
 } 
 
+//CONFIGURANDO A MATRIZ DE LEDS com a biblioteca ws2818b equivalente a ws2812b
 
-// Definição da estrutura de cor para cada LED
+// Definição da estrutura de cor para cada LED da matriz
 struct pixel_t {
     uint8_t R, G, B;
 };
 typedef struct pixel_t npLED_t;
 npLED_t leds[NUM_LEDS];
 
-// PIO e state machine para controle dos LEDs
+// PIO e state machine para controle dos LEDs 
 PIO np_pio;
 uint sm;
 
@@ -77,7 +89,7 @@ void bf() {
     sleep_us(100);
 }
 
-// Função de controle inicial da matriz de LEDs
+// Função de controle inicial da matriz de LEDs com a ws2818b.
 void controle(uint pino) {
     uint offset = pio_add_program(pio0, &ws2818b_program);
     np_pio = pio0;
@@ -97,7 +109,7 @@ void cor(const uint indice, const uint8_t r, const uint8_t g, const uint8_t b) {
     leds[indice].B = b;
 }
 
-// Funções para os diferentes efeitos
+//DESENHANDO CADA NÚMERO
 void number0() {
     int mat1[5][5][3] = {
         {{0, 0, 0}, {BRILHO, 0, 0}, {BRILHO, 0, 0}, {BRILHO, 0, 0},{0, 0, 0}},
@@ -213,7 +225,7 @@ void number5() {
 
 void number6() {
     int mat1[5][5][3] = {
-        {{0, 0, 0}, {0, 0, BRILHO}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+        {{0, 0, 0}, {0, 0, BRILHO}, {0, 0, BRILHO}, {0, 0, BRILHO}, {0, 0, 0}},
         {{0, 0, 0}, {0, 0, BRILHO}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
         {{0, 0, 0}, {0, 0, BRILHO}, {0, 0, BRILHO}, {0, 0, BRILHO}, {0, 0, 0}},
         {{0, 0, 0}, {0, 0, BRILHO}, {0, 0, 0}, {0, 0, BRILHO}, {0, 0, 0}},
@@ -270,11 +282,12 @@ void number8() {
 
 void number9() {
     int mat1[5][5][3] = {
-        {{0, 0, 0}, {255, 255, 255}, {255, 255, 255}, {255, 255, 255}, {0, 0, 0}},
-        {{0, 0, 0}, {255, 255, 255}, {0, 0, 0}, {255, 255, 255}, {0, 0, 0}},
-        {{0, 0, 0}, {255, 255, 255}, {255, 255, 255}, {255, 255, 255}, {0, 0, 0}},
-        {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {255, 255, 255}, {0, 0, 0}},
-        {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {255, 255, 255}, {0, 0, 0}}
+        {{0, 0, 0}, {BRILHO, BRILHO, BRILHO}, {BRILHO, BRILHO, BRILHO}, {BRILHO, BRILHO, BRILHO}, {0, 0, 0}},
+        {{0, 0, 0}, {BRILHO, BRILHO, BRILHO}, {0, 0, 0}, {BRILHO, BRILHO, BRILHO}, {0, 0, 0}},
+        {{0, 0, 0}, {BRILHO, BRILHO, BRILHO}, {BRILHO, BRILHO, BRILHO}, {BRILHO, BRILHO, BRILHO}, {0, 0, 0}},
+        {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {BRILHO, BRILHO, BRILHO}, {0, 0, 0}},
+        {{0, 0, 0}, {BRILHO, BRILHO, BRILHO}, {BRILHO, BRILHO, BRILHO}, {BRILHO, BRILHO, BRILHO}, {0, 0, 0}}
+
     };
 
     // Exibir a matriz
@@ -287,6 +300,7 @@ void number9() {
     bf();
 }
 
+// switch para atualizar o estado de cada número
 void atualizar_estado() {
     switch (estado_atual) {
         case 0:
@@ -322,6 +336,10 @@ void atualizar_estado() {
     }
 }
 
+// Aplicando as rotinhas de interrupção IRQ para corrigir o debounce.
+/* Aproveitando a função para criar uma estrutura condicional para incrementar e
+   decrementar um número da função ATUALIZAR_ESTADO lendo o estado atual sempre que
+   um dos botões forem pressionados. */
 void debounce_botao(uint pino, volatile uint32_t *last_irq_time, int direcao) {
     uint32_t tempo_atual = to_ms_since_boot(get_absolute_time());
     if (tempo_atual - *last_irq_time > DEBOUNCE_DELAY) {
@@ -332,22 +350,34 @@ void debounce_botao(uint pino, volatile uint32_t *last_irq_time, int direcao) {
         atualizar_estado();
     }
 }
-// Função main
-int main() {
-    controle(PINO_MATRIZ);
+
+// Função para inicializar os pinos dos botões como entrada IN;
+void iniciar_botoes() {
     gpio_init(BOTAO_A);
     gpio_init(BOTAO_B);
     gpio_set_dir(BOTAO_A, GPIO_IN);
     gpio_set_dir(BOTAO_B, GPIO_IN);
     gpio_pull_up(BOTAO_A);
     gpio_pull_up(BOTAO_B);
-    number0();
+}
+// Função principal
+int main() {
+    controle(PINO_MATRIZ); //Controle do pino 7
+    iniciar_botoes(); // Permite a interação com os botões
+    number0(); //Define o estado inicial da matriz de leds como o número 0
 
     while (1) {
-        state(1, 0, 0, 180);
-        state(0, 0, 0, 20);
+        state(1, 0, 0, 180); // O led vermelho vermelho tipo RGB fica 180 ms ligado
+        state(0, 0, 0, 20);  // Todos os leds ficam 20 ms desligados
+
+        // Ativa a interrupção do botão B e decrementa um estado.
         if (!gpio_get(BOTAO_B)) debounce_botao(BOTAO_B, &last_irq_time_B, -1);
+        //Ativa a interrupção do botão A e incrementa um estado.
         if (!gpio_get(BOTAO_A)) debounce_botao(BOTAO_A, &last_irq_time_A, +1);
-        sleep_ms(10);
+        sleep_ms(10); // Aguarda 10 ms para a próxima leitura.
+
+        /* O próximo ciclo será sempre depois de 210 ms devido ao led ficar 180 ms ligado
+        10 ms desligado e o aguardo de 10 ms para o próximo ciclo*/
     }
+    return (0); //Este ponto nunca será alcançado.
 }
